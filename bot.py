5472bf1afa578e567d4d1f470122e3e311475564
Leticia-hanjisung <leleca.haikyuu@gmail.com>
2026-02-23 16:09:27 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import random
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,4 +36,44 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
-bot.run("MTQ2Nzk3MzY4MjU1ODQwMjgzNA.GObq4x.Dl1xsKgpzWzgWmjkN0Ral0BeIEiVWGtMkt0WUo")
+class MyClient(discord.Client):
+
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await guild.system_channel.send(to_send)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+class MyClient(discord.Client):
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send(f'Oops. It is actually {answer}.')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot.run("MTQ2Nzk3MzY4MjU1ODQwMjgzNA.GydEuV.sveebT_61rQjrZE_lmGdWKDDg54zW4yWIWaY60")
